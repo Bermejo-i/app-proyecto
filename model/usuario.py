5f394d20abc73ac45e024bef7c90fc588106e5aa
= <=raulcznc@gmail.com>
2024-07-12 21:36:11 -0500

    inicial

---
+++ model/usuario.py
@@ -0,0 +1,11 @@
+class usuario:
+
+    def __init__(self,cod_trab,tipoTrab,telefono,direccion,correo,edad,cargo):
+        self.cod_trab=cod_trab
+        self.tipoTrab=tipoTrab
+        self.telefono=telefono
+        self.direccion=direccion
+        self.correo=correo
+        self.edad=edad
+        self.cargo=cargo
+    
